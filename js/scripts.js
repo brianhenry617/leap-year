8fe2b975f538f56ecf717d4c48a32c273b62c8d2
Brian Henry and CJ Rhone <cj@>
2018-03-28 09:29:05 -0700

    Add backend logic, it will make the the text field return as false

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// BUSINESS logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+
+
+});
+});
+// USER INTERFACE
+var leapYear = function(year) {
+      return false;
+};
